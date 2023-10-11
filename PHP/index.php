59951af2cc37e2921751ce3353b9b8d79b4728f6
heroinsFormula <lukyostrihon@gmail.com>
2023-10-11 14:38:55 +0200

    vše zobrazeno

--- PHP/index.php
+++ PHP/index.php
@@ -19,7 +19,59 @@ session_start();
                         <th></th>
                     </tr>
                 <?php
-                $query = "SELECT autor, nazev, rok_vydani, puvod, literarni_druh FROM tituly";
+                $query = "SELECT * FROM tituly WHERE literarni_druh='Próza'";
+                $result = mysqli_query($conn,$query);            
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row["autor"] . "</td>";
+                echo "<td>" . $row["nazev"] . "</td>";
+                echo "<td>" . $row["rok_vydani"] . "</td>";
+                echo "<td>" . $row["puvod"] . "</td>";
+                echo "<td><input type='checkbox'></td>";
+                echo "</tr>";                    
+                }
+                ?>
+                </table>
+            </div>
+            <div class="table">
+                <h1 class="title">Drama</h1>
+                <table>
+                    <tr>
+                        <th>Autor</th>
+                        <th>Název</th>
+                        <th>Rok vydání</th>
+                        <th>Původ</th>
+                        <th></th>
+                    </tr>
+                <?php
+                $query = "SELECT * FROM tituly WHERE literarni_druh='Drama'";
+                $result = mysqli_query($conn,$query);           
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row["autor"] . "</td>";
+                echo "<td>" . $row["nazev"] . "</td>";
+                echo "<td>" . $row["rok_vydani"] . "</td>";
+                echo "<td>" . $row["puvod"] . "</td>";
+                echo "<td><input type='checkbox'></td>";
+                echo "</tr>";                    
+                }
+                ?>
+                </table>
+            </div>
+            <div class="table">
+                <h1 class="title">Poezie</h1>
+                <table>
+                    <tr>
+                        <th>Autor</th>
+                        <th>Název</th>
+                        <th>Rok vydání</th>
+                        <th>Původ</th>
+                        <th></th>
+                    </tr>
+                <?php
+                $query = "SELECT * FROM tituly WHERE literarni_druh='Poezie'";
                 $result = mysqli_query($conn,$query);            
 
                 while ($row = mysqli_fetch_assoc($result)) {
