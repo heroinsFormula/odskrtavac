21edb05d5e3986c00bb97b992d2298ab211da2a1
heroinsFormula <lukyostrihon@gmail.com>
2023-10-21 16:44:01 +0200

    do databáze se to posílá jinak když je to checked = true a false

--- JS/index.js
+++ JS/index.js
@@ -5,13 +5,20 @@ const xhttp = new XMLHttpRequest();
 function handleChange(checkbox) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-        if (checkbox.checked === true) { 
-            alert(checkbox.id);    
-        } else{
-            alert(checkbox.id);
-        }
+        // if (checkbox.checked === true) { 
+            
+        // } else{
+
+        // }
         }
     };
-    xhttp.open("GET", "add_to_db.php?id="+checkbox.id);
-    xhttp.send();
+    console.log(checkbox.checked);
+    if (checkbox.checked === true) {
+        var params = `id=${checkbox.id}&checked=true`;
+    } else if (checkbox.checked === false) {
+        var params = `id=${checkbox.id}&checked=false`;
+    }    
+    xhttp.open("POST", "add_to_db.php");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    xhttp.send(params);
 }
--- PHP/add_to_db.php
+++ PHP/add_to_db.php
@@ -1,10 +1,17 @@
 <?php
-$book_id = intval($_GET['id']);
+$book_id = intval($_POST['id']);
+$checked = strval($_POST['checked']);
 include("functions.php");
 if (!$conn) {
     die('Could not connect: ' . mysqli_error($con));
   }
 mysqli_select_db($conn,"odskrtavac");
+if ($checked === "true") {
 $sql="INSERT INTO read_books(book_id) SELECT book_id FROM tituly WHERE book_id = '".$book_id."'";
+} 
+else if ($checked === "false") {
+$sql="INSERT INTO read_books(read_book) VALUES('".$checked."')";
+}
+
 $result = mysqli_query($conn,$sql);
 ?>
