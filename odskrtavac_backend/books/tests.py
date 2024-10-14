18cfc6e0272c5e3d08087328961c6404f3063b62
heroinsFormula <lukyostrihon@gmail.com>
2024-10-14 23:24:30 +0200

    API posílá údaje o kritériích, včetně duplicitních autorů

--- odskrtavac_backend/books/tests.py
+++ odskrtavac_backend/books/tests.py
@@ -8,8 +8,18 @@ from django.contrib.auth.models import User
 
 class BookTestCase(APITestCase):
     def setUp(self):
-        Book.objects.create(name='můj kemp')
+        self.author1 = Author.objects.create(full_name="John Doe", country="GB")
+        self.author2 = Author.objects.create(country="CZ")
+        Book.objects.create(name='můj kemp', author=self.author1, publish_year=0)
+        Book.objects.create(name='1', author=self.author1, publish_year=0)
+        Book.objects.create(name='2', author=self.author1, publish_year=0)
+        Book.objects.create(name='3', author=self.author1, publish_year=0)
+
         User.objects.create_user(username='test_user', password='password123')
+
+
+
+
 
     def login_user(self, username='test_user', password='password123'):
         url = reverse('user_api:login')
@@ -49,3 +59,76 @@ class BookTestCase(APITestCase):
         """
         response = self.mark_book(slug='blbost').get('error')
         self.assertEqual(response, 'Kniha nebyla nalezena')
+
+
+
+    def test_18_stoleti(self):
+        """
+        1701-1800 => 18. století
+        """
+        Book.objects.create(name='18stol', 
+                            publish_year=1800,
+                            author=self.author1)
+        response = self.mark_book(slug='18stol').get('criteria')
+        self.assertEqual(response['Světová a česká do 18. století'], 1)
+    
+    def test_19_stoleti(self):
+        """
+        1801-1900 => 19. století
+        """
+        Book.objects.create(name='19stol', 
+                            publish_year=1900,
+                            author=self.author1)
+        response = self.mark_book(slug='19stol').get('criteria')
+        self.assertEqual(response['Světová a česká 19. století'], 1)
+
+    def test_svet(self):
+        """
+        1901-202x, author GB
+        """
+        Book.objects.create(name='svet', 
+                            publish_year=1901,
+                            author=self.author1)
+        response = self.mark_book(slug='svet').get('criteria')
+        self.assertEqual(response['Světová 20. a 21. století'], 1)
+
+    def test_cz(self):
+        """
+        1901-202x, author CZ
+        """
+        Book.objects.create(name='cz', 
+                            publish_year=1901,
+                            author=self.author2)
+        response = self.mark_book(slug='cz').get('criteria')
+        self.assertEqual(response['Česká 20. a 21. století'], 1)
+
+    def test_proza(self):
+        Book.objects.create(name='x', 
+                            publish_year=0,
+                            literary_type='Próza',
+                            author=self.author1)
+        response = self.mark_book(slug='x').get('criteria')
+        self.assertEqual(response['Próza'], 1)
+    
+    def test_poezie(self):
+        Book.objects.create(name='x', 
+                            publish_year=0,
+                            literary_type='Poezie',
+                            author=self.author1)
+        response = self.mark_book(slug='x').get('criteria')
+        self.assertEqual(response['Poezie'], 1)    
+
+    def test_drama(self):
+        Book.objects.create(name='x', 
+                            publish_year=0,
+                            literary_type='Drama',
+                            author=self.author1)
+        response = self.mark_book(slug='x').get('criteria')
+        self.assertEqual(response['Drama'], 1)
+
+    def test_duplicitni_autor(self):
+        self.mark_book(slug='1')
+        self.mark_book(slug='2')
+        response = self.mark_book(slug='3').get('criteria')
+        self.assertEqual(response['Duplicitní autoři'][0], 'John Doe')
+
--- odskrtavac_backend/books/views.py
+++ odskrtavac_backend/books/views.py
@@ -11,6 +11,7 @@ from books.serializer import AuthorSerializer, BookSerializer
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 
+from collections import Counter
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -35,6 +36,56 @@ def get_authors(request):
     serializer = AuthorSerializer(authors, many=True)
     return Response(serializer.data)
 
+def evaluate_book_requirements(user):
+    books = user.read_books.all()
+    criteria = {
+        "Světová a česká do 18. století": 0,
+        "Světová a česká 19. století": 0,
+        "Světová 20. a 21. století": 0,
+        "Česká 20. a 21. století": 0,
+        "Próza":0,
+        "Poezie":0,
+        "Drama":0,
+        "Celkem": 0,
+        "Duplicitní autoři":[]
+    }
+    authors = []
+    criteria["Celkem"] = len(books)
+
+    for book in books:
+        publish_year = book.publish_year
+        literary_type = book.literary_type
+        author = book.author
+        country = author.country
+        
+        authors.append(author)
+
+        if publish_year <= 1800:
+            criteria["Světová a česká do 18. století"] += 1
+        elif 1801 <= publish_year <= 1900:
+            criteria["Světová a česká 19. století"] += 1
+        elif country != "CZ":
+            criteria["Světová 20. a 21. století"] += 1
+        elif country == "CZ":
+            criteria["Česká 20. a 21. století"] += 1
+
+        if literary_type == "Próza":
+            criteria["Próza"] += 1
+        elif literary_type == "Poezie":
+            criteria["Poezie"] += 1
+        elif literary_type == "Drama":
+            criteria["Drama"] += 1
+
+    count_authors = Counter(authors)
+    for author, occurrences in count_authors.items():
+        if occurrences > 2:
+            criteria["Duplicitní autoři"].append(author.full_name)
+
+
+    return criteria
+
+
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def toggle_read_status(request, slug):
@@ -49,8 +100,10 @@ def toggle_read_status(request, slug):
     else:
         book.read_by.add(request.user)
         read_status = True
+    
+    criteria = evaluate_book_requirements(request.user)
 
-    return JsonResponse({'slug': slug, 'read': read_status})
+    return JsonResponse({'slug': slug, 'read': read_status, 'criteria': criteria})
 
 
 class IndexView(generic.ListView):
